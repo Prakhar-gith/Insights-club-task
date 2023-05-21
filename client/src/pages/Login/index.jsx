d5ac9c1225568f8bd95f72094d15e155b7993e05
Prakhar-gith <115631222+Prakhar-gith@users.noreply.github.com>
2023-05-21 23:37:17 +0530

    Initial commit

---
+++ client/src/pages/Login/index.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+import styles from "./styles.module.css";
+
+function Login() {
+	const googleAuth = () => {
+		window.open(
+			`${process.env.REACT_APP_API_URL}/auth/google/callback`,
+			"_self"
+		);
+	};
+	return (
+		<div className={styles.container}>
+			<h1 className={styles.heading}>Log in </h1>
+			<div className={styles.form_container}>
+				<div className={styles.left}>
+					<img className={styles.img} src="./images/login.jpg" alt="login" />
+				</div>
+				<div className={styles.right}>
+					<h2 className={styles.from_heading}>Members Log in</h2>
+					<input type="text" className={styles.input} placeholder="Email" />
+					<input type="text" className={styles.input} placeholder="Password" />
+					<a href="https://insights-club-home.netlify.app" className={styles.btn}>Log in</a>
+					{/* <button className={styles.btn} onclick="location.href='https://insights-club-home.netlify.app/'" type="button">Log In</button> */}
+					<p className={styles.text}>or</p>
+					<button className={styles.google_btn} onClick={googleAuth}>
+						<img src="./images/google.png" alt="google icon" />
+						<span>Sign in with Google</span>
+					</button>
+					<p className={styles.text}>
+						New User? Click to <Link to="/signup">Sign Up</Link>
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Login;
